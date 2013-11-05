3841362afeb3e2d8f4855e12475455cb41c9efc0
Nicholas Chin <nchin2014@kellogg.northwestern.edu>
2013-11-05 17:54:10 -0600

    Completed homework#6

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,39 @@
 class PicturesController < ApplicationController
   def show
+
+  	@picture = Picture.find_by(:id => params[:id])
   end
+
+  def index
+  	@list_of_pics = Picture.all
+  end	
+
+  def new
+  end
+
+  def create
+  	picture = Picture.new
+  	picture.source = params[:source]
+  	picture.caption = params[:caption]
+  	picture.save
+  end
+
+  def destroy
+  	picture = Picture.find_by(:id => params[:id])
+  	picture.destroy
+  end
+
+  def edit
+  	@picture = Picture.find_by(:id => params[:id])
+  end
+
+  def update
+  	@picture = Picture.find_by(:id => params[:id])
+  	@picture.source = params[:source]
+  	@picture.caption = params[:caption]
+  	@picture.save
+  end
+
+
+
 end
